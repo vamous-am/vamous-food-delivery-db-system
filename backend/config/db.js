e5a8215e4124a2bc649574dc64bbfa8ce4b26ac0
vamous-am <amanuelmusa11@gmail.com>
2026-05-16 07:41:25 +0300

    Prep the MVP for Cloud Deployment Configured database connection for Aiven cloud SSL

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,8 +7,15 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
     logging: false,
+    dialectOptions: process.env.NODE_ENV === 'production' ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    } : {}
   }
 );
 
